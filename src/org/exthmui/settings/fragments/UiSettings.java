f1675e2e37a701b5d2fff83e21454170060d3f4e
Pranav Vashi <neobuddy89@gmail.com>
2019-10-13 19:26:46 +0530

    exthmui: Bring up UI fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: cjybyjk <cjybyjk@gmail.com>

--- src/org/exthmui/settings/fragments/UiSettings.java
+++ src/org/exthmui/settings/fragments/UiSettings.java
@@ -1,37 +1,95 @@
+/*
+ * Copyright (C) 2016-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.exthmui.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import android.preference.SwitchPreference;
-import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
-import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.exthmui.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
 
-public class UiSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+import org.exthmui.settings.R;
+import org.exthmui.settings.fragments.ui.Animations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lineageos.providers.LineageSettings;
+
+public class UiSettings extends SettingsPreferenceFragment implements Indexable {
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.exthm_settings_ui);
-
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-
-        return false;
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Animations.reset(mContext);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.EXTHMUI_SETTINGS;
     }
+
+    /**
+     * For search
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.exthm_settings_ui;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+
+                    return keys;
+                }
+            };
 }
