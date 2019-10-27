1b9757a86fd6f61f73b393ec9ef39eaa0985fd6d
Pranav Vashi <neobuddy89@gmail.com>
2019-10-27 21:35:00 +0530

    Add battery styles and customizations for Android 10 [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    exthmui: Update drawable for battery percent when charging
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/exthmui/settings/fragments/StatusBarSettings.java
+++ src/org/exthmui/settings/fragments/StatusBarSettings.java
@@ -132,6 +132,12 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                 Settings.System.ROAMING_INDICATOR_ICON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_FOURG_ICON, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_BATTERY_TEXT_CHARGING, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
         BatteryBar.reset(mContext);
         Clock.reset(mContext);
         NetworkTrafficSettings.reset(mContext);
