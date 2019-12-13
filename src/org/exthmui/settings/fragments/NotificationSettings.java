5d0fadc1b69aa8436f4746428c228148eaaf458a
Lup Gabriel <gwolf2u.website@gmail.com>
2019-12-13 19:10:21 +0200

    exTHm Settings: Add kill button to notification guts [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/exthmui/settings/fragments/NotificationSettings.java
+++ src/org/exthmui/settings/fragments/NotificationSettings.java
@@ -91,6 +91,8 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         Settings.System.putIntForUser(resolver,
                 Settings.System.LESS_BORING_HEADS_UP, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.NOTIFICATION_GUTS_KILL_APP_BUTTON, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1, UserHandle.USER_CURRENT);
     }
 
