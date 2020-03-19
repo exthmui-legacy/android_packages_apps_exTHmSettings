e0a0ab43665fa935d984ce6084711ee70ffaf78c
cjybyjk <cjybyjk@gmail.com>
2020-03-19 23:21:50 +0800

    exthmui: add theme settings
    based on crDroid
    commit 34c1296e7aee0cb9cb7a92f720ded7ee818c7a44
    commit cef542f1b85a52a1b9c4c493531f668ab7ffbae5
    commit 42882853e6466cd230b539a03f0ba5813319635a
    commit a7956e41358624209d0b72e2baa0e1a30dee25d9
    commit 107f69540a119ec95e49cab84e2461492b613864
    
    Signed-off-by: cjybyjk <cjybyjk@gmail.com>

--- src/org/exthmui/settings/fragments/ui/ThemeSettings.java
+++ src/org/exthmui/settings/fragments/ui/ThemeSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2016-2019 crDroid Android Project
+ * Copyright (C) 2018-2019 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,19 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.exthmui.settings.fragments;
 
-import android.content.BroadcastReceiver;
+package org.exthmui.settings.fragments.ui;
+
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.os.SystemProperties;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -36,35 +34,42 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.exthmui.Utils;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
 import org.exthmui.settings.R;
-import org.exthmui.settings.fragments.ui.Animations;
-import org.exthmui.settings.fragments.ui.RoundedCorners;
-import org.exthmui.settings.fragments.ui.ThemeSettings;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-import lineageos.providers.LineageSettings;
+public class ThemeSettings extends SettingsPreferenceFragment implements Indexable,
+        Preference.OnPreferenceChangeListener {
 
-public class UiSettings extends SettingsPreferenceFragment implements Indexable {
+    public static final String TAG = "ThemeSettings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.exthm_settings_ui);
+        addPreferencesFromResource(R.xml.theme_settings);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
-        Animations.reset(mContext);
-        RoundedCorners.reset(mContext);
-        ThemeSettings.reset(mContext);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.BERRY_DARK_STYLE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
@@ -83,17 +88,10 @@ public class UiSettings extends SettingsPreferenceFragment implements Indexable 
                     ArrayList<SearchIndexableResource> result =
                             new ArrayList<SearchIndexableResource>();
                     SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.exthm_settings_ui;
+                    sir.xmlResId = R.xml.theme_settings;
                     result.add(sir);
 
                     return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-
-                    return keys;
                 }
             };
 }
