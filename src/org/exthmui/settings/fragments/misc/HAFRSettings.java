crdroidandroid/android_packages_apps_crDroidSettings

Add ability to permanently hide apps from recents [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/misc/HAFRSettings.java
@@ -1,0 +1,313 @@
+/*
+ * Copyright (C) 2020 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.fragments.misc;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import androidx.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.crdroid.settings.R;
+import com.crdroid.settings.preferences.PackageListAdapter;
+import com.crdroid.settings.preferences.PackageListAdapter.PackageItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HAFRSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceClickListener {
+
+    private static final int DIALOG_HIDE_APPS = 1;
+
+    private PackageListAdapter mPackageAdapter;
+    private PackageManager mPackageManager;
+    private PreferenceGroup mHideAppPrefList;
+    private Preference mHideAppPref;
+
+    private String mHiddenPackageList;
+    private Map<String, Package> mHiddenPackages;
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get launch-able applications
+        addPreferencesFromResource(R.xml.hafr_settings);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.hide_apps_from_recents_summary);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPackageManager = getPackageManager();
+        mPackageAdapter = new PackageListAdapter(getActivity());
+
+        mHideAppPrefList = (PreferenceGroup) findPreference("hafr_applications");
+        mHideAppPrefList.setOrderingAsAdded(false);
+
+        mHiddenPackages = new HashMap<String, Package>();
+
+        mHideAppPref = findPreference("hafr_packages");
+
+        mHideAppPref.setOnPreferenceClickListener(this);
+
+        mContext = getActivity().getApplicationContext();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshCustomApplicationPrefs();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_HIDE_APPS) {
+            return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+        }
+        return 0;
+    }
+
+    /**
+     * Utility classes and supporting methods
+     */
+    @Override
+    public Dialog onCreateDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Dialog dialog;
+        final ListView list = new ListView(getActivity());
+        list.setAdapter(mPackageAdapter);
+
+        builder.setTitle(R.string.profile_choose_app);
+        builder.setView(list);
+        dialog = builder.create();
+
+        switch (id) {
+            case DIALOG_HIDE_APPS:
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        addCustomApplicationPref(info.packageName, mHiddenPackages);
+                        dialog.cancel();
+                    }
+                });
+        }
+        return dialog;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putStringForUser(resolver,
+                Settings.System.HIDE_FROM_RECENTS_LIST, "", UserHandle.USER_CURRENT);
+    }
+
+    /**
+     * Application class
+     */
+    private static class Package {
+        public String name;
+        /**
+         * Stores all the application values in one call
+         * @param name
+         */
+        public Package(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(name);
+            return builder.toString();
+        }
+
+        public static Package fromString(String value) {
+            if (TextUtils.isEmpty(value)) {
+                return null;
+            }
+
+            try {
+                Package item = new Package(value);
+                return item;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    };
+
+    private void refreshCustomApplicationPrefs() {
+        if (!parsePackageList()) {
+            return;
+        }
+
+        // Add the Application Preferences
+        if (mHideAppPrefList != null) {
+            mHideAppPrefList.removeAll();
+
+            for (Package pkg : mHiddenPackages.values()) {
+                try {
+                    Preference pref = createPreferenceFromInfo(pkg);
+                    mHideAppPrefList.addPreference(pref);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Do nothing
+                }
+            }
+
+            // Keep these at the top
+            mHideAppPref.setOrder(0);
+            // Add 'add' options
+            mHideAppPrefList.addPreference(mHideAppPref);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference == mHideAppPref) {
+            showDialog(DIALOG_HIDE_APPS);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_message)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (preference == mHideAppPrefList.findPreference(preference.getKey())) {
+                                removeApplicationPref(preference.getKey(), mHiddenPackages);
+                            }
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null);
+
+            builder.show();
+        }
+        return true;
+    }
+
+    private void addCustomApplicationPref(String packageName, Map<String,Package> map) {
+        Package pkg = map.get(packageName);
+        if (pkg == null) {
+            pkg = new Package(packageName);
+            map.put(packageName, pkg);
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private Preference createPreferenceFromInfo(Package pkg)
+            throws PackageManager.NameNotFoundException {
+        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
+                PackageManager.GET_META_DATA);
+        Preference pref =
+                new Preference(getActivity());
+
+        pref.setKey(pkg.name);
+        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
+        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
+        pref.setPersistent(false);
+        pref.setOnPreferenceClickListener(this);
+        return pref;
+    }
+
+    private void removeApplicationPref(String packageName, Map<String,Package> map) {
+        if (map.remove(packageName) != null) {
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private boolean parsePackageList() {
+        boolean parsed = false;
+
+        final String hafrString = Settings.System.getString(getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST);
+
+        if (!TextUtils.equals(mHiddenPackageList, hafrString)) {
+            mHiddenPackageList = hafrString;
+            mHiddenPackages.clear();
+            parseAndAddToMap(hafrString, mHiddenPackages);
+            parsed = true;
+        }
+
+        return parsed;
+    }
+
+    private void parseAndAddToMap(String baseString, Map<String,Package> map) {
+        if (baseString == null) {
+            return;
+        }
+
+        final String[] array = TextUtils.split(baseString, "\\|");
+        for (String item : array) {
+            if (TextUtils.isEmpty(item)) {
+                continue;
+            }
+            Package pkg = Package.fromString(item);
+            map.put(pkg.name, pkg);
+        }
+    }
+
+
+    private void savePackageList(boolean preferencesUpdated, Map<String,Package> map) {
+        List<String> settings = new ArrayList<String>();
+        for (Package app : map.values()) {
+            settings.add(app.toString());
+        }
+        final String value = TextUtils.join("|", settings);
+        if (preferencesUpdated) {
+            mHiddenPackageList = value;
+        }
+        Settings.System.putString(getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST, value);
+    }
+}

exthmui-legacy/android_packages_apps_exTHmSettings

src/org/exthmui/settings/fragments/misc/HAFRSettings.java
@@ -1,0 +1,313 @@
+/*
+ * Copyright (C) 2020 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.exthmui.settings.fragments.misc;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import androidx.preference.SwitchPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import org.exthmui.settings.R;
+import org.exthmui.settings.preferences.PackageListAdapter;
+import org.exthmui.settings.preferences.PackageListAdapter.PackageItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HAFRSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceClickListener {
+
+    private static final int DIALOG_HIDE_APPS = 1;
+
+    private PackageListAdapter mPackageAdapter;
+    private PackageManager mPackageManager;
+    private PreferenceGroup mHideAppPrefList;
+    private Preference mHideAppPref;
+
+    private String mHiddenPackageList;
+    private Map<String, Package> mHiddenPackages;
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get launch-able applications
+        addPreferencesFromResource(R.xml.hafr_settings);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.hide_apps_from_recents_summary);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPackageManager = getPackageManager();
+        mPackageAdapter = new PackageListAdapter(getActivity());
+
+        mHideAppPrefList = (PreferenceGroup) findPreference("hafr_applications");
+        mHideAppPrefList.setOrderingAsAdded(false);
+
+        mHiddenPackages = new HashMap<String, Package>();
+
+        mHideAppPref = findPreference("hafr_packages");
+
+        mHideAppPref.setOnPreferenceClickListener(this);
+
+        mContext = getActivity().getApplicationContext();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshCustomApplicationPrefs();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTHMUI_SETTINGS;
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_HIDE_APPS) {
+            return MetricsProto.MetricsEvent.EXTHMUI_SETTINGS;
+        }
+        return 0;
+    }
+
+    /**
+     * Utility classes and supporting methods
+     */
+    @Override
+    public Dialog onCreateDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Dialog dialog;
+        final ListView list = new ListView(getActivity());
+        list.setAdapter(mPackageAdapter);
+
+        builder.setTitle(R.string.profile_choose_app);
+        builder.setView(list);
+        dialog = builder.create();
+
+        switch (id) {
+            case DIALOG_HIDE_APPS:
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        addCustomApplicationPref(info.packageName, mHiddenPackages);
+                        dialog.cancel();
+                    }
+                });
+        }
+        return dialog;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putStringForUser(resolver,
+                Settings.System.HIDE_FROM_RECENTS_LIST, "", UserHandle.USER_CURRENT);
+    }
+
+    /**
+     * Application class
+     */
+    private static class Package {
+        public String name;
+        /**
+         * Stores all the application values in one call
+         * @param name
+         */
+        public Package(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(name);
+            return builder.toString();
+        }
+
+        public static Package fromString(String value) {
+            if (TextUtils.isEmpty(value)) {
+                return null;
+            }
+
+            try {
+                Package item = new Package(value);
+                return item;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    };
+
+    private void refreshCustomApplicationPrefs() {
+        if (!parsePackageList()) {
+            return;
+        }
+
+        // Add the Application Preferences
+        if (mHideAppPrefList != null) {
+            mHideAppPrefList.removeAll();
+
+            for (Package pkg : mHiddenPackages.values()) {
+                try {
+                    Preference pref = createPreferenceFromInfo(pkg);
+                    mHideAppPrefList.addPreference(pref);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Do nothing
+                }
+            }
+
+            // Keep these at the top
+            mHideAppPref.setOrder(0);
+            // Add 'add' options
+            mHideAppPrefList.addPreference(mHideAppPref);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference == mHideAppPref) {
+            showDialog(DIALOG_HIDE_APPS);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_message)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (preference == mHideAppPrefList.findPreference(preference.getKey())) {
+                                removeApplicationPref(preference.getKey(), mHiddenPackages);
+                            }
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null);
+
+            builder.show();
+        }
+        return true;
+    }
+
+    private void addCustomApplicationPref(String packageName, Map<String,Package> map) {
+        Package pkg = map.get(packageName);
+        if (pkg == null) {
+            pkg = new Package(packageName);
+            map.put(packageName, pkg);
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private Preference createPreferenceFromInfo(Package pkg)
+            throws PackageManager.NameNotFoundException {
+        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
+                PackageManager.GET_META_DATA);
+        Preference pref =
+                new Preference(getActivity());
+
+        pref.setKey(pkg.name);
+        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
+        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
+        pref.setPersistent(false);
+        pref.setOnPreferenceClickListener(this);
+        return pref;
+    }
+
+    private void removeApplicationPref(String packageName, Map<String,Package> map) {
+        if (map.remove(packageName) != null) {
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private boolean parsePackageList() {
+        boolean parsed = false;
+
+        final String hafrString = Settings.System.getString(getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST);
+
+        if (!TextUtils.equals(mHiddenPackageList, hafrString)) {
+            mHiddenPackageList = hafrString;
+            mHiddenPackages.clear();
+            parseAndAddToMap(hafrString, mHiddenPackages);
+            parsed = true;
+        }
+
+        return parsed;
+    }
+
+    private void parseAndAddToMap(String baseString, Map<String,Package> map) {
+        if (baseString == null) {
+            return;
+        }
+
+        final String[] array = TextUtils.split(baseString, "\\|");
+        for (String item : array) {
+            if (TextUtils.isEmpty(item)) {
+                continue;
+            }
+            Package pkg = Package.fromString(item);
+            map.put(pkg.name, pkg);
+        }
+    }
+
+
+    private void savePackageList(boolean preferencesUpdated, Map<String,Package> map) {
+        List<String> settings = new ArrayList<String>();
+        for (Package app : map.values()) {
+            settings.add(app.toString());
+        }
+        final String value = TextUtils.join("|", settings);
+        if (preferencesUpdated) {
+            mHiddenPackageList = value;
+        }
+        Settings.System.putString(getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST, value);
+    }
+}