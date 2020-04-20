ef374d4284683bb6662403a0f54bbf0ead74ffb4
cjybyjk <cjybyjk@gmail.com>
2020-04-20 12:52:10 +0800

    powermenu: remove ScreenRecord prefreence
    Revert "crdroid: Remove ScreenRecord preference if not supported"
    
    This reverts commit 400021b6f7b6b317fd8887876c9fcc3436d31ebd.
    
    Revert "Add power menu option for screen record [3/3]"
    
    This reverts commit 8dcb5a0ddc45a3109273779d0941f25462c524e8.

--- src/org/exthmui/settings/fragments/buttons/PowerMenuActions.java
+++ src/org/exthmui/settings/fragments/buttons/PowerMenuActions.java
@@ -46,12 +46,9 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     final static String TAG = "PowerMenuActions";
 
     private SwitchPreference mScreenshotPref;
-    private SwitchPreference mScreenrecordPref;
     private SwitchPreference mAirplanePref;
     private SwitchPreference mUsersPref;
     private SwitchPreference mLockDownPref;
-
-    private boolean mScreenRecordAvailable;
 
     Context mContext;
     private ArrayList<String> mLocalUserConfig = new ArrayList<String>();
@@ -64,16 +61,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.power_menu_settings);
         mContext = getActivity().getApplicationContext();
 
-        mScreenRecordAvailable = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_showScreenRecord);
-
         mAllActions = PowerMenuConstants.getAllActions();
 
         for (String action : mAllActions) {
             if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
                 mScreenshotPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
-            } else if (action.equals(GLOBAL_ACTION_KEY_SCREENRECORD)) {
-                mScreenrecordPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENRECORD);
             } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
                 mAirplanePref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
             } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
@@ -92,14 +84,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         if (mScreenshotPref != null) {
             mScreenshotPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENSHOT));
-        }
-
-        if (mScreenrecordPref != null) {
-            if (!mScreenRecordAvailable) {
-                getPreferenceScreen().removePreference(mScreenrecordPref);
-            } else {
-                mScreenrecordPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENRECORD));
-            }
         }
 
         if (mAirplanePref != null) {
@@ -127,10 +111,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         if (preference == mScreenshotPref) {
             value = mScreenshotPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
-
-        } else if (preference == mScreenrecordPref) {
-            value = mScreenrecordPref.isChecked();
-            updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENRECORD);
 
         } else if (preference == mAirplanePref) {
             value = mAirplanePref.isChecked();
